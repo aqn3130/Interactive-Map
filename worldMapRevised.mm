7ba42f015cb5c830a77956da481531378d1bed36
Ahmad Qurbanzada <ahmad.qurbanzada@outlook.com>
2018-01-31 09:32:17 +0000

    refactor

---
+++ worldMapRevised.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1516619350700" ID="ID_1024313231" MODIFIED="1516619350700" TEXT="New Mindmap"/>
+</map>
